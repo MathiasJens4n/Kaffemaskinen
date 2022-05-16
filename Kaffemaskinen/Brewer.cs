7d51bbf9ab6333cb144e6230b465bed25ffdd45f
Mathias Jensen <mathiasbeckj@gmail.com>
2022-05-16 22:53:01 +0200

    Add project files.

---
+++ Kaffemaskinen/Brewer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffemaskinen
+{
+    internal class Brewer: IFIlter, IPowder, IPower, IWater, ICofee, IEspresso
+    {
+        public string Cups { get; set; }
+        public string Process { get; set; }
+        public string AddWater(int cups)
+        {
+            return ($"Pouring water for {cups} cups");
+        }
+        public string InsertFilter()
+        {
+            return ("Inserting filter");
+        }
+        public string AddPowder()
+        {
+            return ("Adding powder");
+        }
+        public string TurnOn()
+        {
+            return ("Turning on machine");
+        }
+
+        public string MakeCoffee(int cups)
+        {
+            Process += $"{AddWater(cups)}\n";
+            Process += $"{InsertFilter()}\n";
+            Process += $"{AddPowder()}\n";
+            Process += $"{TurnOn()}\n";
+            Process += $"Making coffee\n";
+            return Process;
+        }
+        public string MakeTea(int cups)
+        {
+            Process += $"{AddWater(cups)}\n";
+            Process += $"{InsertFilter()}\n";
+            Process += $"{AddPowder()}\n";
+            Process += $"{TurnOn()}\n";
+            Process += $"Making Tea\n";
+            return Process;
+        }
+
+        public string MakeEspresso()
+        {
+            Process += $"{AddWater(1)}\n";
+            Process += $"{InsertFilter()}\n";
+            Process += $"{AddPowder()}\n";
+            Process += $"{TurnOn()}\n";
+            Process += $"Making Espresso\n";
+            return Process;
+
+        }
+    }
+}
---
+++ Kaffemaskinen/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffemaskinen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Brewer brewer = new Brewer();
+            Console.WriteLine(brewer.MakeEspresso());
+        }
+    }
+}
